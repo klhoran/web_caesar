012e870f81c1d7bd9ba500d8a147e8d35ace92c8
Kimberly Horan <klhoran@ymail.com>
2017-03-25 10:06:10 -0700

    add text area box and submit button

--- main.py
+++ main.py
@@ -20,8 +20,13 @@ import caesar
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         message = "Hellloooo world!"
-        encrypted_message = encrypt(message, 13)
-        self.response.write(message)
+        encrypted_message = caesar.encrypt(message, 13)
+
+        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        submit = "<input type = 'submit'/>"
+        form = "<form>" + textarea + "<br>" + submit + "</form>"
+
+        self.response.write(form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
